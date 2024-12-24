27df7abd70a83186fc13235ddc7f0346d93c5c98
Muhammad Suleman <145265998+SulemanWaraich@users.noreply.github.com>
2024-12-24 22:46:54 +0500

    part 2 (ugly solution)

--- Week 3/index.js
+++ Week 3/index.js
@@ -2,28 +2,31 @@ const express = require("express")
 const app = express();
 const port = 3001;
 
-
-app.get("/health-checkup", (req, res)=>{
-  const kidneyId = req.query.kidneyId;
-  const name = req.headers.name;
-  const password = req.headers.password;
-
-  console.log(name, kidneyId);
-  console.log(password);
-
+function headers(name, password){
   if(name !== "Suleman" || password !== "pass"){
     res.status(403).json({
       msg: "User doesn't exist."
     })
     return;
   }
+}
 
+function query(kidneyId){
   if(kidneyId != 1 && kidneyId != 2){
     res.status(401).json({
       msg: 'wrong inputs'
     });
     return;
   }
+} 
+
+app.get("/health-checkup", (req, res)=>{
+  const kidneyId = req.query.kidneyId;
+  const name = req.headers.name;
+  const password = req.headers.password;
+
+  headers(name, password);
+  query(kidneyId);
 
   res.send("your'e fine.")
 })
