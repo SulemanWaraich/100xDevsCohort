a25cc325f49f1ac731fb9491413c8e134ed7d4a1
Muhammad Suleman <145265998+SulemanWaraich@users.noreply.github.com>
2024-12-25 02:02:22 +0500

    Global Catches

--- Week 3/index.js
+++ Week 3/index.js
@@ -1,32 +1,21 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const port = 3001;
+const port = 3000;
 
-function userMiddleware(req, res, next){
-  if(req.headers.name !== "Suleman" || req.headers.password !== "pass"){
-    res.status(403).json({
-      msg: "User doesn't exist."
-    })
-    return;
-  }else{
-    next();
-  }
-}
 
-function kidneyMiddleware(req, res, next){
-  if(req.query.kidneyId != 1 && req.query.kidneyId != 2){
-    res.status(401).json({
-      msg: 'wrong inputs'
-    });
-    return;
-  }else{
-    next();
-  }
-} 
+app.use(express.json());
+app.post("/health-checkup", (req, res)=>{
+  const kidneys = req.body.kidneys;
+  const kidneyLength = kidneys.length
 
-app.get("/health-checkup", userMiddleware, kidneyMiddleware, (req, res)=>{
+  console.log(kidneys);
+  console.log(kidneyLength);
+  
+  res.send("your'e fine." + kidneyLength)
+})
 
-  res.send("your'e fine.")
+app.use((err,req,res,next) => {
+  res.status(500).send("An internal error occurred");
 })
 
 app.listen(port, ()=>{
