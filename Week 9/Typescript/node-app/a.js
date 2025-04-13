3941c5ad8251fe2c93de3e1b370ce8db378c2399
Muhammad Suleman <145265998+SulemanWaraich@users.noreply.github.com>
2025-04-13 22:52:58 +0500

    Typescript

---
+++ Week 9/Typescript/node-app/a.js
@@ -0,0 +1,8 @@
+"use strict";
+const x = 1;
+console.log(x);
+function sum(a, b) {
+    return a + b;
+}
+const res = sum(4, 5);
+console.log(res);
---
+++ Week 9/Typescript/node-app/a.tsx
@@ -0,0 +1,10 @@
+const x: number = 1;
+console.log(x);
+
+
+function sum(a: number ,b: number){
+  return a + b
+}
+
+const res = sum(4,5)
+console.log(res);
