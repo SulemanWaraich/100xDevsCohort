05cd338008812b7f96887cd8a8ddcbd22d32ecd5
Muhammad Suleman <145265998+SulemanWaraich@users.noreply.github.com>
2025-07-23 16:20:44 +0500

    next.js

---
+++ Week 14/next/app/auth/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ({children}: {children: React.ReactNode}) {
+  return (
+    <div>
+      <div>
+        get 20% off.....!
+      </div>
+      {children}
+    </div>
+  )
+}
+
+// export default layout
